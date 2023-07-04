3c7d8655d0b3bd0cbb79c8cef43b0b7107dc75c3
maria <antonova.maria.a@gmail.com>
2023-07-04 21:02:17 +0300

    task 4 improvement

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,6 +1,12 @@
 ﻿int[,,] Fillmatrix(int x, int y, int z, int min, int max)
 {
+    int[] array = new int [90];
+    for (int i = 0; i<array.Length; i++){
+        array[i] = i+10;
+    }
+
     int[,,] matrix = new int[x, y, z];
+    int temp = 0;
     Random rand = new Random();
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
@@ -8,7 +14,11 @@
         {
             for (int f = 0; f < matrix.GetLength(2); f++)
             {
-                matrix[i, j, f] = rand.Next(min, max + 1);
+                temp = rand.Next(0, array.Length);
+                matrix[i, j, f] = array[temp];
+                array = array.Except(new int[] { temp }).ToArray();
+
+                //matrix[i, j, f] = rand.Next(min, max + 1);//не придумала, как поменять генерацию на неповторяющуюся
             }
         }
     }
@@ -25,7 +35,7 @@ void Printmatrix(int[,,] matrix)
             {
                 System.Console.Write(matrix[i, j, f] + "[ " + i + " " + j + " " + f + " ]" + "\t");
             }
-            
+
         }
         System.Console.WriteLine();
     }
@@ -43,8 +53,21 @@ int Input(string text)
 int x = Input("input x: "); // columns
 int y = Input("input y: "); // rows
 int z = Input("input z: ");
-int minnum = Input("input minimal number: ");
-int maxnum = Input("input maximum number: ");
-int[,,] matrix = Fillmatrix(x, y, z, minnum, maxnum);
-Printmatrix(matrix);
-System.Console.WriteLine();
+
+
+
+if (x * y * z > 99)
+{
+    System.Console.WriteLine("error. more indexes than unrepetitive numbers. try again");
+}
+else
+{
+    int minnum = Input("input minimal number: ");
+    int maxnum = Input("input maximum number: ");
+    int[,,] matrix = Fillmatrix(x, y, z, minnum, maxnum);
+    Printmatrix(matrix);
+    System.Console.WriteLine();
+}
+
+
+
